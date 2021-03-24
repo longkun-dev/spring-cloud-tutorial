6bba6cf128712faab3ec284135a357862ec8da9a
766521466@qq.com <766521466@qq.com>
2021-03-25 01:07:03 +0800

    config server and config client

---
+++ config-client/src/main/java/com/cloud/configclient/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.cloud.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author :  zhulongkun20@gmail.com
+ * @create :  03-25-2021 00:48:16
+ * @description :  配置中心测试
+ * @since :  v1.0
+ */
+@RestController
+@RequestMapping("/config/client")
+public class TestController {
+
+    @Value("${config.param1}")
+    private String configParam1;
+
+    @GetMapping(value = "/config/param1")
+    public String configParam1() {
+        return configParam1;
+    }
+}
---
+++ config-server/src/main/java/com/cloud/configserver/controller/ConfigTestController.java
@@ -0,0 +1,25 @@
+package com.cloud.configserver.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author :  zhulongkun20@gmail.com
+ * @create :  03-25-2021 00:29:41
+ * @description :  配置中心配置读取测试
+ * @since :  v1.0
+ */
+@RestController
+@RequestMapping("/config/server")
+public class ConfigTestController {
+
+//    @Value("${config.param1}")
+    private String configParam1;
+
+    @GetMapping("/param1")
+    public String param1() {
+        return configParam1;
+    }
+}
